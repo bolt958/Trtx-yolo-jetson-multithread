4b8276e7ad145d886309690ec54d026525e238a3
LianJiang Ma <82355332+bolt958@users.noreply.github.com>
2024-06-21 09:18:29 +0800

    Add files via upload

---
+++ opencv_gst_c++.cpp
@@ -0,0 +1,37 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+
+int main()
+{
+    std::string rtsp_url = "rtsp://192.168.0.210:8557/PSIA/Streaming/channels/2?videoCodecType=H.264"; 
+    std::string pipeline = "rtspsrc location=" + rtsp_url + " ! rtph264depay ! h264parse ! omxh264dec ! nvvidconv ! video/x-raw, width=960, height=540, format=BGRx ! videoconvert ! appsink sync=false";
+    std::cout << pipeline << std::endl;
+
+    cv::VideoCapture cap = cv::VideoCapture(pipeline, cv::CAP_GSTREAMER);
+
+
+    if(!cap.isOpened())
+    {
+        std::cerr << "error to open camera." << std::endl;
+        return -1;
+    }
+    std::cout << cv::getBuildInformation() << std::endl;
+    cv::Mat frame ;
+
+
+    while(1)
+    {
+        bool ret = cap.read(frame);
+        if(ret)
+        {
+            cv::imshow("Frame",frame);
+            if (cv::waitKey(1) == 27)   
+                break;
+        }
+
+    }
+    cv::destroyWindow("Frame");
+    cap.release();
+    return 0;
+}
